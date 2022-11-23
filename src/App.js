ab170a01c614c2fa7e027b4043b5cf853cb4fffe
Kuba Sobecki <gosustyle@gmail.com>
2022-11-23 14:33:47 +0100

    Use async/await instead of then

--- src/App.js
+++ src/App.js
@@ -6,20 +6,19 @@ import './App.css';
 function App() {
     const [movies, setMovies] = useState([]);
 
-    const fetchMoviesHandler = () => {
-        fetch('https://swapi.py4e.com/api/films/')
-            .then(res => res.json())
-            .then(data => {
-                const transformedMovies = data.results.map(movie => {
-                    return {
-                        id: new Date(movie.edited).getTime(),
-                        title: movie.title,
-                        openingText: movie.opening_crawl,
-                        releaseDate: movie.release_date
-                    };
-                });
-                setMovies(transformedMovies);
-            });
+    const fetchMoviesHandler = async () => {
+        const response = await fetch('https://swapi.py4e.com/api/films/');
+        const data = await response.json();
+
+        const transformedMovies = data.results.map(movie => {
+            return {
+                id: new Date(movie.edited).getTime(),
+                title: movie.title,
+                openingText: movie.opening_crawl,
+                releaseDate: movie.release_date
+            };
+        });
+        setMovies(transformedMovies);
     };
 
     return (
