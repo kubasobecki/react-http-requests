1931f6b293826470c4e7217714c03e40a1c10b37
Kuba Sobecki <gosustyle@gmail.com>
2022-11-23 14:16:20 +0100

    Fetch mkovies data from swapi

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+    const [movies, setMovies] = useState([]);
 
-  return (
-    <React.Fragment>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
-    </React.Fragment>
-  );
+    const fetchMoviesHandler = () => {
+        fetch('https://swapi.py4e.com/api/films/')
+            .then(res => res.json())
+            .then(data => {
+                const transformedMovies = data.results.map(movie => {
+                    return {
+                        id: new Date(movie.edited).getTime(),
+                        title: movie.title,
+                        openingText: movie.opening_crawl,
+                        releaseDate: movie.release_date
+                    };
+                });
+                setMovies(transformedMovies);
+            });
+    };
+
+    return (
+        <React.Fragment>
+            <section>
+                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+            </section>
+            <section>
+                <MoviesList movies={movies} />
+            </section>
+        </React.Fragment>
+    );
 }
 
 export default App;
